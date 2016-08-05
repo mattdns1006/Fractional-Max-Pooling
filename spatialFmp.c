16cd757ab89d340ca5f4cefcb961be32e4f212af
snow_leps <mattdns100689@gmail.com>
2016-08-05 17:59:31 +0100

    Added sequenced gen for 2d

--- spatialFmp.c
+++ spatialFmp.c
@@ -27,41 +27,51 @@ struct Matrix *newMatrix(long w, long h)
 
 void matrixPrint(struct Matrix *m){
 	int idx, i,j;
-	int gap = 4;
+	int gap = 10;
 
 	for(j=0; j< m->h; j++){
 		for(i=0; i< m->w; i++){
 			idx = i + j*m->w;
-			//printf("(%d, %d) ",i,j);
-			printf("%*f",gap,m->matrix[idx]);
+			printf("%*f ",gap,m->matrix[idx]);
 		}
 		printf("\n");
 	}
+	printf("\n\n");
+}
+
+
+long* generateIntervals(float u, long nIn, long nOut, int poolSize){
+	float alpha;
+	alpha = (float)(nIn - poolSize)/(float)(nOut - 1);
+	long *sequence = (long*)malloc(sizeof(long)*nOut+1);
+	long i;
+	sequence[0] = 0;
+	sequence[nOut+1] = nIn;
+	for(i=1;i<=nOut ;i++){
+		sequence[i] = ceil(alpha*(i+u))-1;
+	}
+	return sequence;
 }
 
 
 int main(void){
 
 	int poolSize = 2;
-	long inputSize = 100;
-	float *input, *output, *gradInput;
+	long inputW = 10;
+	long inputH = 10;
 	float fmp = 0.7;
-	long outputSize = floor(inputSize*fmp);
+	long outputH = floor(inputH*fmp);
+	long outputW = floor(inputW*fmp);
 	float u = 0.5;
-	printf("inSize = %ld, ouSize = %ld, fraction = %f. \n",inputSize,outputSize,fmp);
 
-	size_t inBytes = sizeof(float)*inputSize;
-	size_t outBytes = sizeof(float)*outputSize;
-	input = (float*)malloc(inBytes);
-	output = (float*)malloc(outBytes);
-	gradInput = (float*)malloc(inBytes);
+	struct Matrix *input = newMatrix(inputW,inputH);
+	struct Matrix *output = newMatrix(outputW,outputH);
+	matrixPrint(input);
+	matrixPrint(output);
 
-	struct Matrix *m1 = newMatrix(20,8);
-	matrixPrint(m1);
+	long *sequenceH, *sequenceW;
+	sequenceW = generateIntervals(u,inputW,outputW,poolSize);
+	sequenceH = generateIntervals(u,inputH,outputH,poolSize);
 
-
-	free(input);
-	free(output);
-	free(gradInput);
 	return 0;
 }
