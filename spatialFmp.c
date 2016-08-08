38f1fec994f7036a8beb7d52dddee0bc9e729360
snow_leps <mattdns100689@gmail.com>
2016-08-08 20:23:35 +0100

    fixed spatialFmp

--- spatialFmp.c
+++ spatialFmp.c
@@ -20,7 +20,7 @@ struct Matrix *newMatrix(long w, long h)
 	for(j=0; j< m->h; j++){
 		for(i=0; i< m->w; i++){
 			idx = i + j*m->w;
-			m->matrix[idx] = sin(idx);
+			m->matrix[idx] = idx + sin(idx);
 		}
 	}
 	return m;
@@ -43,27 +43,45 @@ void matrixPrint(struct Matrix *m){
 
 struct Sequence 
 {
-	long nIn;
-	long nOut;
+	long nInH;
+	long nInW;
+	long nOutH;
+	long nOutW;
 	int poolSize;
-	long *sequence;
+	long *sequenceW;
+	long *sequenceH;
 };
 
-struct Sequence *newSequence(long nIn, long nOut, int poolSize)
+struct Sequence *newSequence(long nInW, long nOutW, long nInH, long nOutH, int poolSize)
 {
 	struct Sequence *s = malloc(sizeof(struct Sequence));
-	s->nIn = nIn;
-	s->nOut = nOut;
+
+	s->nInW = nInW;
+	s->nOutW = nOutW;
+	s->nInH = nInH;
+	s->nOutH = nOutH;
 	s->poolSize = poolSize;
-	s->sequence = (long*)malloc(sizeof(long)*(nOut+1));
-	float alpha;
-	alpha = (float)(nIn - poolSize)/(float)(nOut - 1);
-	long i;
+	size_t size = sizeof(long);
+
+	s->sequenceW = (long*)malloc(size*(nOutW));
+	s->sequenceH = (long*)malloc(size*(nOutH));
+
+	float alphaW, alphaH;
+	alphaW = (float)(nInW - poolSize)/(float)(nOutW - 1);
+	alphaH = (float)(nInH - poolSize)/(float)(nOutH - 1);
+	
 	float u = 0.5;
-	s->sequence[0] = 0;
-	s->sequence[nOut+1] = nIn - 1;
-	for(i=1;i <= s->nOut ;i++){
-		s->sequence[i] = ceil(alpha*(i+u))-1;
+
+	long i;
+
+	s->sequenceW[0] = 0;
+	for(i=1;i < s->nOutW ;i++){
+		s->sequenceW[i] = ceil(alphaW*(i+u))-1;
+	}
+
+	s->sequenceH[0] = 0;
+	for(i=1;i < s->nOutH ;i++){
+		s->sequenceH[i] = ceil(alphaH*(i+u))-1;
 	}
 	return s;
 };
@@ -71,30 +89,58 @@ struct Sequence *newSequence(long nIn, long nOut, int poolSize)
 void sequencePrint(struct Sequence *s){
 	int gap = 5;
 	long i;
-	printf("Indices Sequence\n");
-	for(i=0;i<=s->nOut;i++){
+	printf("Indices Sequences width \n");
+	for(i=0;i<s->nOutW;i++){
 		printf("%*ld ",gap,i);
-		printf("%*ld ",gap, s->sequence[i]);
+		printf("%*ld ",gap, s->sequenceW[i]);
+	printf("\n");
+	}
+	printf("Indices Sequences height \n");
+	for(i=0;i<s->nOutH;i++){
+		printf("%*ld ",gap,i);
+		printf("%*ld ",gap, s->sequenceH[i]);
 	printf("\n");
 	}
 	printf("\n");
 }
 
 void forwardInput(struct Matrix *input, struct Matrix *output, struct Sequence *sequence){
-	long i,j,idx,idx2;
+	long i,j;
+	long idx,idx1;
 	long i1,j1;
 	long startW, startH;
 	int gap =5;
 	int poolSize = sequence-> poolSize;
-	float maxVal;
+	float maxVal,val;
+	long *seqH = sequence->sequenceH;
+	long *seqW = sequence->sequenceW;
 
 	for(j=0;j<output->h;j++){
 		for(i=0;i<output->w;i++){
 
 			idx = i + j*output->w;
+			//printf("i,j,idx = %lu %lu \n",i,j,idx);
+
 			startW = i;
 			startH = j;
-			maxVal = -100;
+			maxVal = input->matrix[idx];
+			for(j1=0; j1 < poolSize; ++j1){
+				for(i1=0; i1 < poolSize; ++i1 ){
+
+
+					long x = seqW[i] + i1;	
+					long y = seqH[j] + j1;	
+					idx1 = x + y*input->w;
+					val = input->matrix[idx1];
+					
+					//printf("i1,j1,idx,val(idx) = %lu, %lu, %lu, %f \n",x,y,idx1,val);
+					if (val > maxVal)
+						maxVal = val;
+
+				}
+			}
+			//printf("Max value = %f \n\n",maxVal);
+
 			output->matrix[idx] = maxVal;
 				
 		}
@@ -107,8 +153,8 @@ void forwardInput(struct Matrix *input, struct Matrix *output, struct Sequence *
 int main(void){
 
 	int poolSize = 2;
-	long inputW = 15;
-	long inputH = inputW; // Make everything square to begin with
+	long inputW = 5;
+	long inputH = 4; // Make everything square to begin with
 	float fmp = 0.6;
 	long outputH = floor(inputH*fmp);
 	long outputW = floor(inputW*fmp);
@@ -119,7 +165,7 @@ int main(void){
 	matrixPrint(input);
 	matrixPrint(output);
 
-	struct Sequence *sequenceH = newSequence(inputH,outputH,poolSize);
+	struct Sequence *sequenceH = newSequence(inputW, outputW, inputH, outputH, poolSize);
 	sequencePrint(sequenceH);
 
 	forwardInput(input,output,sequenceH);
